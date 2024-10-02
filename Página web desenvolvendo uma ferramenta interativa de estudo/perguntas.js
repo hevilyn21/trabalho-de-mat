91478186b2e871570d9b8591fc5160921dd87bd4
hevilyn21 <hevilyn.abreu.oliveira@escola.pr.gov.br>
2024-10-02 13:48:21 -0300

    Add files via upload

---
+++ Página web desenvolvendo uma ferramenta interativa de estudo/perguntas.js
@@ -0,0 +1,65 @@
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'Quando a internet foi criada?', //pergunta
+    'A internet foi criada na década de 1960, com o desenvolvimento da ARPANET, que se tornou operacional em 1969.' //resposta
+)
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que são cabos de transmissão intercontinentais de internet e qual é a sua velocidade de transmissão?', //pergunta
+    'Cabos de transmissão intercontinentais de internet são cabos submarinos que conectam continentes, com velocidades de transmissão que podem ultrapassar 400 Tbps (terabits por segundo).' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Inteligêngia Artificial', //tópico da pergunta
+    'O que é Inteligência Artificial?', //pergunta
+    'Inteligência Artificial é a área da ciência da computação que desenvolve sistemas capazes de realizar tarefas que normalmente requerem inteligência humana, como aprendizado, reconhecimento e tomada de decisões.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'o que são servidores?', //pergunta
+    'Servidores são computadores ou sistemas que fornecem serviços, dados ou recursos a outros dispositivos em uma rede, como armazenamento, processamento ou hospedagem de sites.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Inteligêngia Artificial', //tópico da pergunta
+    'Qual é a primeira IA a ser criada? Quando e por quem? Qual era seu objetivo ?', //pergunta
+    'A primeira IA, chamada "Logic Theorist," foi criada em 1955 por Allen Newell e Herbert A. Simon, com o objetivo de provar teoremas matemáticos. Ela marcou o início da pesquisa em inteligência artificial.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Inteligêngia Artificial', //tópico da pergunta
+    'Qual é o impacto da inteligência artificial, no mundo atual?', //pergunta
+    'A inteligência artificial está revolucionando setores como saúde, indústria e serviços, otimizando processos e criando novas soluções. No entanto, também levanta questões éticas sobre emprego, privacidade e segurança.' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que são roteadores?', //pergunta
+    'Roteadores são dispositivos que direcionam o tráfego de dados entre redes de computadores, permitindo a comunicação entre dispositivos conectados.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'O que é cibercrimes?', //pergunta
+    'Cibercrimes são atividades ilegais cometidas através de dispositivos digitais ou da internet, visando roubar, fraudar ou prejudicar pessoas ou sistemas.' //resposta
+)
+
+
+criaCartao( //cria o cartao 
+    'Cibercrimes', //tópico da pergunta
+    'Cite exemplos de cibercrimes?', //pergunta
+    'Exemplos de cibercrimes incluem roubo de identidade, fraudes financeiras, invasão de sistemas, phishing, e disseminação de malware, como vírus e ransomware. Esses crimes visam comprometer dados ou causar danos digitais.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'LGPD', //tópico da pergunta
+    'Qual é o objetivo da LGPD?', //pergunta
+    'O objetivo da LGPD é proteger a privacidade e os direitos dos titulares de dados, regulando o uso responsável de suas informações pessoais.' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'LGPD', //tópico da pergunta
+    ' O que é LGPD?', //pergunta
+    'A Lei Geral de Proteção de Dados (LGPD) é a legislação brasileira que regula o tratamento de dados pessoais, garantindo a privacidade e segurança das informações. Ela se aplica a empresas e organizações que coletam ou processam dados no Brasil.' //resposta
+)
